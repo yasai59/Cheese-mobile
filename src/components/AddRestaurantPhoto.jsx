53492d237b58d1ab4ae4708234547c16320b00aa
Yasai <alexvidalcasado@gmail.com>
2024-03-04 10:00:17 +0100

    [UPDATE] restaurant photo picker

--- src/components/AddRestaurantPhoto.jsx
+++ src/components/AddRestaurantPhoto.jsx
@@ -1,14 +1,52 @@
-import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import tw from "../../twrnc";
 import { Feather } from "@expo/vector-icons";
+import * as DocumentPicker from "expo-document-picker";
 
-export const AddRestaurantPhoto = () => {
+export const AddRestaurantPhoto = ({ setImageDef }) => {
+  const [image, setImage] = useState(null);
+
+  const handlePickImage = async () => {
+    const result = await DocumentPicker.getDocumentAsync({
+      type: "image/*",
+      copyToCacheDirectory: true,
+    });
+    if (!result.canceled) {
+      setImage(result.assets[0]);
+
+      setImageDef && setImageDef(result.assets[0]);
+    }
+  };
+
   return (
-    <TouchableOpacity
-      style={tw`rounded-full border-2 aspect-square border-dashed border-light w-30 self-center justify-center items-center mt-3`}
-    >
-      <Feather name="plus" size={50} color={tw`text-light`.color} />
-    </TouchableOpacity>
+    <>
+      <TouchableOpacity
+        style={tw`rounded-full border-2 aspect-square ${
+          !image ? "border-dashed border-light" : ""
+        }  w-30 self-center justify-center items-center mt-3`}
+        onPress={handlePickImage}
+      >
+        {image ? (
+          <View style={tw`absolute w-full h-full`}>
+            <Image
+              source={{ uri: image.uri }}
+              style={tw`w-full h-full rounded-full`}
+            />
+          </View>
+        ) : (
+          <Feather name="plus" size={50} color={tw`text-light`.color} />
+        )}
+      </TouchableOpacity>
+      {/* delete image */}
+      {image && (
+        <Text
+          style={tw`text-red-500 text-center`}
+          onPress={() => setImage(null) && setImageDef && setImageDef(null)}
+        >
+          Delete image
+        </Text>
+      )}
+    </>
   );
 };
